3aea0164d398ab1514ca006d2b638804bb164238
madhav-1810 <142152369+madhav-1810@users.noreply.github.com>
2023-11-06 21:59:14 +0530

    Add files via upload

---
+++ P4/output/p4-11.cpp
@@ -0,0 +1,17 @@
+// parameterized constructor. 
+#include<iostream>
+using namespace std;
+class area{
+	public:
+		area(int length,int breath)
+		{
+			cout<<"Area of rectangle : "<<length*breath;
+		}
+};
+int main()
+{
+	cout<<"I am Madhav vasani "<<endl;
+	cout<<"En_no    220130318118 "<<endl;
+	cout<<"---------------------"<<endl;
+	area(3,8);
+}
